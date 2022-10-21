bcccaaf3741d9755b0bee1b3219c511b92dc6ce5
Karlos <karin_ar72@hotmail.com>
2022-10-21 11:14:31 -0500

    1st version of functionality onmouseover to show paragraph's img

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// Nav mouseover events
-const links = document.querySelector('#nav');
+// Home's nav mouseover events
+/*const links = document.querySelector('#nav');
 links.addEventListener('mouseover', function(e) {
 	if(e.target.matches('.links')) {
 		e.target.style.backgroundColor= 'black';
@@ -10,5 +10,13 @@ links.addEventListener('mouseout', function(e) {
 	if(e.target.matches('.links')) {
 		e.target.style.backgroundColor= 'transparent';
 	}
-});
+});*/
 
+
+let body = document.querySelector('body');
+let picts = document.querySelector('img');
+let legend = document.querySelector('p');
+body.addEventListener('mouseover', function(e) {
+	if(e.target.matches('img')) {
+		legend.style.display= 'block';
+	}})
