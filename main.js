c1739c9b647af64f3d143c0a59ec5ae2b05bc77f
Karlos <karin_ar72@hotmail.com>
2022-10-21 11:43:00 -0500

    event listener mouseover/mouseout setted in all the picts

--- main.js
+++ main.js
@@ -17,6 +17,31 @@ let body = document.querySelector('body');
 let picts = document.querySelector('img');
 let legend = document.querySelector('p');
 body.addEventListener('mouseover', function(e) {
-	if(e.target.matches('img')) {
-		legend.style.display= 'block';
-	}})
+	if(e.target.matches('#img1')) {
+		document.querySelector('#pImg1').style.display= 'block';
+	} 
+	else if(e.target.matches('#img2')) {
+		document.querySelector('#pImg2').style.display= 'block'
+	} 
+	else if(e.target.matches('#img3')) {
+		document.querySelector('#pImg3').style.display= 'block'
+	}
+	else if(e.target.matches('#img4')) {
+		document.querySelector('#pImg4').style.display= 'block'
+	}
+})
+
+body.addEventListener('mouseout', function(e) {
+	if(e.target.matches('#img1')) {
+		document.querySelector('#pImg1').style.display= 'none';
+	} 
+	else if(e.target.matches('#img2')) {
+		document.querySelector('#pImg2').style.display= 'none'
+	} 
+	else if(e.target.matches('#img3')) {
+		document.querySelector('#pImg3').style.display= 'none'
+	}
+	else if(e.target.matches('#img4')) {
+		document.querySelector('#pImg4').style.display= 'none'
+	}
+})
