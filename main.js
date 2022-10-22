3cc975ae74a36e8c57ed8924e668e561d0f85117
Karlos <karin_ar72@hotmail.com>
2022-10-22 13:57:03 -0500

    js changed to pseudo-classes

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-// Home's nav mouseover events
-/*const links = document.querySelector('#nav');
-links.addEventListener('mouseover', function(e) {
-	if(e.target.matches('.links')) {
-		e.target.style.backgroundColor= 'black';
-	}
-});
-
-links.addEventListener('mouseout', function(e) {
-	if(e.target.matches('.links')) {
-		e.target.style.backgroundColor= 'transparent';
-	}
-});*/
-
-
+/*
+//Gallery's mouseover/mouseout events
 let body = document.querySelector('body');
 let picts = document.querySelector('img');
 let legend = document.querySelector('p');
