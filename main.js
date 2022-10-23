bfb6b80904f84607131911a7c185c55a7d57a7a8
Karlos <karin_ar72@hotmail.com>
2022-10-23 11:46:17 -0500

    Philosophy's file style and functionality setted

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
-/*
-//Gallery's mouseover/mouseout events
-let body = document.querySelector('body');
-let picts = document.querySelector('img');
-let legend = document.querySelector('p');
-body.addEventListener('mouseover', function(e) {
-	if(e.target.matches('#img1')) {
-		document.querySelector('#pImg1').style.display= 'block';
-	} 
-	else if(e.target.matches('#img2')) {
-		document.querySelector('#pImg2').style.display= 'block'
-	} 
-	else if(e.target.matches('#img3')) {
-		document.querySelector('#pImg3').style.display= 'block'
+let main = document.querySelector('.pages_main');
+let idea1 = document.querySelector('#idea1');
+let modal1 = document.querySelector('#idea1_p');
+let idea2 = document.querySelector('#idea2');
+let modal2 = document.querySelector('#idea2_p');
+let idea3 = document.querySelector('#idea3');
+let modal3 = document.querySelector('#idea3_p');
+
+
+
+main.addEventListener('click', function(e) {
+		if(e.target.id.includes('idea1')) {
+			modal1.style.display= 'block';
+			modal1.style.zIndex= '1';
+		}
+		else if(e.target.id.includes('idea2')) {
+			modal2.style.display= 'block';
+			modal2.style.zIndex= '1';
+		}
+		else if(e.target.id.includes('idea3')) {
+			modal3.style.display= 'block';
+			modal3.style.zIndex= '1';
+		}
+})
+
+main.addEventListener('click', function(e) {
+	if(e.target.id.includes('idea1_p')) {
+		modal1.style.display= 'none';
 	}
-	else if(e.target.matches('#img4')) {
-		document.querySelector('#pImg4').style.display= 'block'
+	else if(e.target.id.includes('idea2_p')) {
+		modal2.style.display= 'none';
+	}
+	else if(e.target.id.includes('idea3_p')) {
+		modal3.style.display= 'none';
 	}
 })
 
-body.addEventListener('mouseout', function(e) {
-	if(e.target.matches('#img1')) {
-		document.querySelector('#pImg1').style.display= 'none';
-	} 
-	else if(e.target.matches('#img2')) {
-		document.querySelector('#pImg2').style.display= 'none'
-	} 
-	else if(e.target.matches('#img3')) {
-		document.querySelector('#pImg3').style.display= 'none'
-	}
-	else if(e.target.matches('#img4')) {
-		document.querySelector('#pImg4').style.display= 'none'
-	}
-})
+
+
