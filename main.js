0d6a34959344b32d97cb4e8317e9e6bdff716b71
Karlos <karin_ar72@hotmail.com>
2022-12-28 10:06:46 -0600

    modal without scroll

--- main.js
+++ main.js
@@ -23,34 +23,41 @@ main.addEventListener('click', function(e) {
 		modal1.style.display= 'block';
 		modal1.style.zIndex= '1';
 		body.style.overflow = 'hidden';
-		
 
 	}
 
 	else if(e.target.id.includes('idea2')) {
 		modal2.style.display= 'block';
 		modal2.style.zIndex= '1';
+		body.style.overflow = 'hidden';
+
 	}
 
 	else if(e.target.id.includes('idea3')) {
 		modal3.style.display= 'block';
 		modal3.style.zIndex= '1';
+		body.style.overflow = 'hidden';
+
 	}
 	else if(e.target.id.includes('idea4')) {
 		modal4.style.display= 'block';
 		modal4.style.zIndex= '1';
+		body.style.overflow = 'hidden';
 	}
 	else if(e.target.id.includes('idea5')) {
 		modal5.style.display= 'block';
 		modal5.style.zIndex= '1';
+		body.style.overflow = 'hidden';
 	}
 	else if(e.target.id.includes('idea6')) {
 		modal6.style.display= 'block';
 		modal6.style.zIndex= '1';
+		body.style.overflow = 'hidden';
 	}
 	else if(e.target.id.includes('idea7')) {
 		modal7.style.display= 'block';
 		modal7.style.zIndex= '1';
+		body.style.overflow = 'hidden';
 	}
 
 });
@@ -65,21 +72,28 @@ main.addEventListener('click', function(e) {
 	
 	else if(e.target.id.includes('idea2_modal')) {
 		modal2.style.display= 'none';
+		body.style.overflow = 'initial';
+
 	}
 	else if(e.target.id.includes('idea3_modal')) {
 		modal3.style.display= 'none';
+		body.style.overflow = 'initial';
 	}
 	else if(e.target.id.includes('idea4_modal')) {
 		modal4.style.display= 'none';
+		body.style.overflow = 'initial';
 	}
 	else if(e.target.id.includes('idea5_modal')) {
 		modal5.style.display= 'none';
+		body.style.overflow = 'initial';
 	}
 	else if(e.target.id.includes('idea6_modal')) {
 		modal6.style.display= 'none';
+		body.style.overflow = 'initial';
 	}
 	else if(e.target.id.includes('idea7_modal')) {
 		modal7.style.display= 'none';
+		body.style.overflow = 'initial';
 	}
 })
 
